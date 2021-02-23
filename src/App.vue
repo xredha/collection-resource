e73e495c1d7adf68a7282b9668199a3ad18579f4
xredha <galihredha100@gmail.com>
2021-02-23 14:05:24 +0700

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li>{{storedResource[0].title}}</li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      storedResource: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          desc: 'You must learn Vue JS in this website',
+          link: 'https://vuejs.org/'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          desc: 'Learn programming in Google',
+          link: 'https://google.com/'
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
